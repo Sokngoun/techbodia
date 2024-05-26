a826d01df926c3b85dbfbedfbf78dd434eed4b3c
Sokngoun <sokngounchan@gmail.com>
2024-05-26 13:03:53 +0700

    search

--- components/CountryCard.tsx
+++ components/CountryCard.tsx
@@ -9,6 +9,15 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import Image from "next/image";
 import { CountryNativeNameType } from "@/lib/types/CountryNativeName";
 
@@ -35,57 +44,114 @@ export default function CountryCard({
 }: CountryCardProps) {
   return (
     <div>
-      <Card className="">
-        <CardHeader className="p-0 border-b-[2px] border-b-gray-100">
-          <Image
-            src={flag!}
-            width={350}
-            height={200}
-            alt=""
-            className="rounded-t-md h-[100px] w-full object-cover"
-          />
-        </CardHeader>
-        <CardContent className="p-1 space-y-2">
-          <CardTitle className="text-sm truncate font-medium">
-            {country_name}
-          </CardTitle>
-          {/* Native Name */}
-          <div className="flex flex-col">
-            <CardDescription className="text-xs truncate font-medium">
-              Native Name:
-            </CardDescription>
-            {native_name!.slice(0, 1).map((native, index) => (
-              <div className="flex flex-col truncate">
-                <CardDescription className="text-xs truncate">
-                  official: {native.official}
+      <Dialog>
+        <DialogTrigger asChild>
+          <Card className="hover:cursor-pointer hover:scale-105 transition ease-in-out duration-200 hover:shadow-lg">
+            <CardHeader className="p-0 border-b-[2px] border-b-gray-100">
+              <Image
+                src={flag!}
+                width={350}
+                height={200}
+                alt=""
+                className="rounded-t-md h-[100px] w-full object-cover"
+              />
+            </CardHeader>
+            <CardContent className="p-1 space-y-2">
+              <CardTitle className="text-sm truncate font-medium">
+                {country_name}
+              </CardTitle>
+              {/* Native Name */}
+              <div className="flex flex-col">
+                <CardDescription className="text-xs truncate font-medium">
+                  Native Name:
+                </CardDescription>
+                {native_name!.slice(0, 1).map((native, index) => (
+                  <div className="flex flex-col truncate">
+                    <CardDescription className="text-xs truncate">
+                      official: {native.official}
+                    </CardDescription>
+                    <CardDescription className="text-xs truncate">
+                      common: {native.common}
+                    </CardDescription>
+                  </div>
+                ))}
+              </div>
+              {/* Alt Spellings */}
+              <div>
+                <CardDescription className="text-xs truncate font-medium">
+                  Alt Spellings:
                 </CardDescription>
                 <CardDescription className="text-xs truncate">
-                  common: {native.common}
+                  {altSpellings}
                 </CardDescription>
               </div>
-            ))}
-          </div>
-          {/* Alt Spellings */}
-          <div>
-            <CardDescription className="text-xs truncate font-medium">
-              Alt Spellings:
-            </CardDescription>
-            <CardDescription className="text-xs truncate">
-              {altSpellings}
-            </CardDescription>
-          </div>
 
-            {/* cca2 cca3 */}
-          <div className="grid grid-cols-2">
-            <CardDescription className="text-xs truncate">
-              <span className="font-medium">cca2:</span> {cca2}
-            </CardDescription>
-            <CardDescription className="text-xs truncate">
-              <span className="font-medium">cca3:</span> {cca3}
-            </CardDescription>
-          </div>
-        </CardContent>
-      </Card>
+              {/* cca2 cca3 */}
+              <div className="grid grid-cols-2">
+                <CardDescription className="text-xs truncate">
+                  <span className="font-medium">cca2:</span> {cca2}
+                </CardDescription>
+                <CardDescription className="text-xs truncate">
+                  <span className="font-medium">cca3:</span> {cca3}
+                </CardDescription>
+              </div>
+            </CardContent>
+          </Card>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <Card className="">
+            <CardHeader className="p-0 border-b-[2px] border-b-gray-100">
+              <Image
+                src={flag!}
+                width={350}
+                height={200}
+                alt=""
+                className="rounded-t-md h-[100px] w-full object-cover"
+              />
+            </CardHeader>
+            <CardContent className="p-1 space-y-2">
+              <CardTitle className="text-sm truncate font-medium">
+                {country_name}
+              </CardTitle>
+              {/* Native Name */}
+              <div className="flex flex-col">
+                <CardDescription className="text-xs truncate font-medium">
+                  Native Name:
+                </CardDescription>
+                {native_name!.slice(0, 1).map((native, index) => (
+                  <div className="flex flex-col truncate">
+                    <CardDescription className="text-xs truncate">
+                      official: {native.official}
+                    </CardDescription>
+                    <CardDescription className="text-xs truncate">
+                      common: {native.common}
+                    </CardDescription>
+                  </div>
+                ))}
+              </div>
+              {/* Alt Spellings */}
+              <div>
+                <CardDescription className="text-xs truncate font-medium">
+                  Alt Spellings:
+                </CardDescription>
+                <CardDescription className="text-xs truncate">
+                  {altSpellings}
+                </CardDescription>
+              </div>
+
+              {/* cca2 cca3 */}
+              <div className="grid grid-cols-2">
+                <CardDescription className="text-xs truncate">
+                  <span className="font-medium">cca2:</span> {cca2}
+                </CardDescription>
+                <CardDescription className="text-xs truncate">
+                  <span className="font-medium">cca3:</span> {cca3}
+                </CardDescription>
+              </div>
+            </CardContent>
+          </Card>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
